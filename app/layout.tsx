4f04c0453712c89e5728e9906e57fd265970d3ac
itzreqle <kingreqle@gmail.com>
2025-02-03 21:03:28 +0330

    Dark mode removed

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className="h-full dark">
+    <html lang="en" className="h-full">
       <body className={cn("relative h-full font-sans antialiased", inter.className)}>
         <main className="relative flex min-h-screen flex-col">{children}</main>
       </body>
